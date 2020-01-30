a73359d3c17b48fdb431ebc9dfe7ba5076d69143
Alvaro Armas <aarmas92@outlook.com>
2020-01-30 21:39:11 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -10,7 +10,7 @@ end
 # call your method here!
 ["Tim", "Tom", "Jim"]
 
-def hello_t(["Tim", "Tom", "Jim"]) do |name|
+defhello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
